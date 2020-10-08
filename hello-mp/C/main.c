fed9d7c899b8c52c3bb6f6ed3b4a47556673a110
mateoochoa <ochoamateol01@gmail.com>
2020-10-08 15:39:41 -0500

    hello

---
+++ hello-mp/C/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int ID = omp_get_thread_num();
+        printf("hello  %d\n", ID);
+        printf("hello again %d\n", ID);
+    }
+//    output:
+//    hello  1
+//    hello  0
+//    hello  3
+//    hello  2
+//    hello again 1
+//    hello again 0
+//    hello again 3
+//    hello again 2
+    return 0;
+}
---
+++ hello-mp/README.md
@@ -0,0 +1,3 @@
+#Hello-MP
+
+Ejemplo básico de uso de la biblioteca openmp donde cada hilo imprime en consola dos mensajes de saludo con sus respectivos identificadores.
