3fe29974ea7f3e4f33a389b44ee4b90a1e5302d5
miguelanruiz <miguelangelrtorresco@gmail.com>
2020-10-23 16:04:27 -0500

    fixed odd multiplication

--- MPP1/algoritmo_3.c
+++ MPP1/algoritmo_3.c
@@ -37,9 +37,9 @@ void Matrix_Init_col(int N, double *a, double *b, double *c)
 /* Inicializacion de matrices */
   for (k=0; k<N; k++)
     for (j=0; j<N; j++) {
-      a[j+k*N] = 2.0*(j+k);
-      b[j+k*N] = 3.2*(j-k);
-      c[j+k*N] = 1.0;
+      a[j+k*N] = 2.0;
+      b[j+k*N] = 1.0;
+      c[j+k*N] = 0.0;
     }
 }
 
@@ -50,8 +50,8 @@ void test(int N, double *a, double *b, double *c)
 printf("A: \n");
   for (k=0; k<N; k++){
     for (j=0; j<N; j++) {
-      a[j+k*N] = j+k;
-      b[j+k*N] = j+k;
+      a[j+k*N] = 2.0;
+      b[j+k*N] = 1.0;
       c[j+k*N] = 0.0;
       printf("%f ", a[j+k*N]);
     }
@@ -60,8 +60,8 @@ printf("A: \n");
   printf("B:\n");
   for (k=0; k<N; k++){
     for (j=0; j<N; j++) {
-      a[j+k*N] = j+k;
-      b[j+k*N] = j+k;
+      a[j+k*N] = 2.0;
+      b[j+k*N] = 1.0;
       c[j+k*N] = 0.0;
       printf("%f ", b[j+k*N]);
     }
@@ -113,31 +113,39 @@ int main (int argc, char **argv)
   c = b + SZ*SZ;
   int num_thread;
 
-  num_thread = mpParallel();
+  num_thread = omp_get_thread_num();
+  printf("Im: %d\n", num_thread);
   /*Solo por master()*/
   #pragma omp master
     Matrix_Init_col(SZ, a, b, c);
 
   /* Invocacion de toma rendimiento (inicio) */  
   mpStart(num_thread);
-
+/*num_thread = omp_get_thread_num();
+  printf("Im: %d\n", num_thread);*/
   /*Inicio de seccion paralela "for" para multiplicacion de matrices*/
 #pragma omp for
-  for (i=0; i<SZ; i=i+2){
+ for (i=0; i<SZ; i=i+2){
     for (j=0; j<SZ; j=j+2) {
       double *pA, *pB, c0, c1, c2, c3;
       c0 = c1 = c2 = c3 = 0.0;
       pA = a+(i*SZ); pB = b+(j*SZ);
       for (k=SZ; k>0; k=k-2, pA = pA+2, pB = pB+2){
         double a0, a1, a2, a3, b0,b1, b2, b3;
-        a0 = *pA; a1 = *(pA+1); a2 = *(pA+SZ); a3 = *(pA+SZ+1);
+	if(k==1){a0 = *pA; a2 = *(pA+SZ); b0 = *pB; b2 = *(pB+SZ); 
+	c0 += a0*b0; c1 += a0*b2;c2 += a2*b0;c3 += a2*b2;}
+	else{
+	a0 = *pA; a1 = *(pA+1); a2 = *(pA+SZ); a3 = *(pA+SZ+1);
         b0 = *pB; b1 = *(pB+1); b2 = *(pB+SZ); b3 = *(pB+SZ+1);
-        c0 += a0*b0 + a1*b1; 
-        c1 += a0*b2 + a1*b3; 
-        c2 += a2*b0 + a3*b1; 
-        c3 += a2*b2 + a3*b3;         
+
+	c0 += a0*b0 + a1*b1; 
+	c1 += a0*b2 + a1*b3; 	
+	c2 += a2*b0 + a3*b1; 	
+	c3 += a2*b2 + a3*b3;  
+	}
       }
       pB = c + i*SZ + j;
+      if(j == SZ-1){*pB = c0; *(pB+SZ) = c2; break;}
       *pB = c0 ;
       *(pB+1) = c1;
       *(pB+SZ) = c2;
@@ -151,5 +159,15 @@ int main (int argc, char **argv)
 }
   /*Invocacion de impresion de funcion */
   mpEnd();
+  printf("Resultados\n");
+  int going = 0;
+  double  *a, *b, *c;
+  a = MEM_CHUNK;
+  b = a + N*N;
+  c = b + N*N;
+  for (int k=0; k<N; k++)
+    for (int j=0; j<N; j++) {
+      printf("%d,%d: %f \n", k, j,c[j+k*N]);
+    }
   return 0;
 }
